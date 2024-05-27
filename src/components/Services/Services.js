3d69da9887a6c59564783925e81662f4142af6a7
Oybek Baxtiyorov <programmeroybek211@gmail.com>
2024-05-27 20:11:04 +0500

    Icon update 2

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -22,10 +22,21 @@ function Services() {
             <ServicesCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
+              title="Web development"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          
+          <Col md={4} className="project-card">
+            <ServicesCard
+              imgPath={editor}
+              isBlog={false}
+              title="Artificial Intelligence"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              ghLink="https://github.com/soumyajit4419/Editor.io"
+              demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
@@ -33,7 +44,7 @@ function Services() {
             <ServicesCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
+              title="CRM system"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
               demoLink="https://blogs.soumya-jit.tech/"
@@ -44,7 +55,33 @@ function Services() {
             <ServicesCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
+              title="Branding"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              ghLink="https://github.com/soumyajit4419/Editor.io"
+              demoLink="https://editor.soumya-jit.tech/"              
+            />
+          </Col>
+
+          
+          <Col md={4} className="project-card">
+            <ServicesCard
+              imgPath={editor}
+              isBlog={false}
+              title="UI&UX"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              ghLink="https://github.com/soumyajit4419/Editor.io"
+              demoLink="https://editor.soumya-jit.tech/"              
+            />
+          </Col>
+
+
+         
+
+          <Col md={4} className="project-card">
+            <ServicesCard
+              imgPath={editor}
+              isBlog={false}
+              title="Mobile apps"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink="https://github.com/soumyajit4419/Editor.io"
               demoLink="https://editor.soumya-jit.tech/"              
